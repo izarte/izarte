49772f3a32f764ad43a4e142f6e6dce1cecaad69
Iñigo Zárate <62849704+izarte@users.noreply.github.com>
2023-09-12 22:06:46 +0200

    Update setup.sh

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -28,8 +28,8 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # customize .zshrc
 # download powerlvl10k and modify zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-curl https://raw.githubusercontent.com/inigo183/inigo183/main/zsh/zshrc > $HOME/.zshrc
-curl https://raw.githubusercontent.com/inigo183/inigo183/main/zsh/p10k.zsh > $HOME/.p10k.zsh
+curl https://raw.githubusercontent.com/izarte/izarte/main/zsh/zshrc > $HOME/.zshrc
+curl https://raw.githubusercontent.com/izarte/izarte/main/zsh/p10k.zsh > $HOME/.p10k.zsh
 
 # customize .zshrc if we are on WSL to use /home/$USER as default location
 wsl_home
